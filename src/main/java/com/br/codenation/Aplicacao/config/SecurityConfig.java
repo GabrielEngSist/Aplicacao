68cdc6978ec7ed2092ad2c83edb4615a629dec37
Gabriel Oliveira Silva <gabriel.engsist@gmail.com>
2019-11-23 13:43:33 -0300

    Adição do Swagger e autenticação com OAuth.

---
+++ src/main/java/com/br/codenation/Aplicacao/config/SecurityConfig.java
@@ -0,0 +1,20 @@
+package com.br.codenation.Aplicacao.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+@EnableWebSecurity
+@EnableAuthorizationServer
+@EnableResourceServer
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    @Override
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
